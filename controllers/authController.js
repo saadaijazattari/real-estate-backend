f9e3e9eb494706aec336b7a42f5266cf7aa3746b
saadaijazattari <saadaijaz1234@>
2026-05-13 13:52:47 +0500

    added login funcitonality with token and cookie=parser

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,4 +1,7 @@
 const bcrypt = require('bcrypt');
+const dotenv = require('dotenv');
+dotenv.config();
+const jwt = require('jsonwebtoken');
 const prisma= require('../lib/prisma')
 
 exports.registerUser= async(req,res) => {
@@ -25,9 +28,55 @@ exports.registerUser= async(req,res) => {
     }
 }
 exports.loginUser= async(req,res) => {
-res.json({
-    name: 'hello'
-})
+    try {
+        const {email, password} = req.body
+        const user = await prisma.user.findUnique({
+            where: {
+                email
+            }
+        })
+        if(!user){
+            return res.status(404).json({
+                success: false,
+                message: "User not found with this email"
+            })
+        }
+        const isPasswordValid = await bcrypt.compare(password, user.password)
+        if(!isPasswordValid){
+            return res.status(401).json({
+                success: false,
+                message: "Invalid password"
+            })
+        }
+
+        const token = jwt.sign({
+            id: user.id,
+            username: user.userName,
+            email: user.email
+        }, process.env.JWT_SECRET, {expiresIn: '5d'})
+        res.cookie('token', token,{
+            httpOnly: true,
+            maxAge: 5 * 24 * 60 * 60 * 1000 
+        })
+        .json({
+            success: true,
+            message: "User logged in successfully",
+            token,
+            user: {
+                id: user.id,
+                username: user.userName,
+                email: user.email
+            }
+        })
+        
+    } catch (error) {
+        res.json({
+            success: false,
+            message: "Error logging in user from server",
+            error: error.message
+        })
+    }
+
 }
 exports.logoutUser= async(req,res) => {
 
